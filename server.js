8d121c1b08cf8ee1320d9cb090827cb978d495bc
zivuch <zivuch@gmail.com>
2023-09-21 14:09:19 +0300

    deploy

--- server.js
+++ server.js
@@ -25,3 +25,13 @@ app.listen(process.env.PORT, () => {
 
 app.use("/api/products", p_router);
 app.use("/api/users", u_router);
+
+
+// Have Node serve the files for our built React app
+// app.use(express.static(path.resolve(__dirname, "./client/build")));
+app.use(express.static(path.join(__dirname, "/client/build")));
+
+// All other GET requests not handled before will return our React app
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
